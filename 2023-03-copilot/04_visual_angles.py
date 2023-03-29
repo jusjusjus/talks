3f3f6b65620081af0320e413a63dd1580a5e443d
Dr. rer. nat. Justus Schwabedal <jschwabedal@gmail.com>
2023-03-29 19:15:05 +0200

    Add(copilot): more info on visual angle demo

--- 2023-03-copilot/04_visual_angles.py
+++ 2023-03-copilot/04_visual_angles.py
@@ -2,5 +2,10 @@
 
 This files defines a function to compute how wide an object on
 a screen can be displayed so that it falls within a given angle
-of visual perception.
+of visual perception.  A character on screen is about 0.21 cm
+wide.  Foveal, parafoveal, and peripheral vision have approximate
+angles of 2, 10, and 60 degrees, respectively.
 """
+import numpy as np
+
+CHARACTER_WIDTH = 0.21  # cm
